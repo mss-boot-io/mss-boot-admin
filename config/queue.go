fb140a3ded4256aa52a1c0736fe121d945ff6f53
lwnmengjing <lwnmengjing@qq.com>
2024-05-16 10:25:41 +0800

    :bug: fix: msk iam (#93)

--- config/queue.go
+++ config/queue.go
@@ -147,7 +147,7 @@ func (m *MSKAccessTokenProvider) Token() (*sarama.AccessToken, error) {
 	if err != nil {
 		return nil, err
 	}
-	m.expired = time.Now().Add(time.Duration(expirationTimeMs) * time.Millisecond).Add(-time.Minute)
+	m.expired = time.UnixMilli(expirationTimeMs).Add(-time.Minute)
 	m.accessToken = &sarama.AccessToken{Token: token}
 	return m.accessToken, nil
 }
