4384841683664d2f836f4b5ca4ae2c3ab2352657
SullivanPrime <sullivanchen93@gmail.com>
2026-08-02 19:28:46 +0800

    feat(generator): wire generated module routes into frontend

--- web/antd/config/routes.custom.ts
+++ web/antd/config/routes.custom.ts
@@ -1,1 +1,4 @@
-export default [];
+import routesGenerated from './routes.generated';
+
+// Put handwritten routes after generated module routes when a custom override is required.
+export default [...routesGenerated];
