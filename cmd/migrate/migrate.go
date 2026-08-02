a34d8078c1d8fba6333fbb2be5ec3a5d711c43e8
SullivanPrime <sullivanchen93@gmail.com>
2026-08-02 19:29:42 +0800

    feat(modules): run registered vertical module migrations

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -18,6 +18,7 @@ import (
 	"github.com/mss-boot-io/mss-boot-admin/config"
 	"github.com/mss-boot-io/mss-boot-admin/middleware"
 	"github.com/mss-boot-io/mss-boot-admin/models"
+	moduleruntime "github.com/mss-boot-io/mss-boot-admin/modules/runtime"
 )
 
 /*
@@ -165,5 +166,5 @@ func migrate() error {
 	migration.Migrate.SetDb(db)
 	migration.Migrate.SetModel(&common.Migration{})
 	migration.Migrate.Migrate()
-	return nil
+	return moduleruntime.Migrate(db)
 }
