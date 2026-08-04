5de059a127156a950516df55d1650ad81d16b4a6
SullivanPrime <sullivanchen93@gmail.com>
2026-08-04 10:21:58 +0800

    test(mss-boot): cover wrapped shutdown cancellation

--- mss-boot/core/server/shutdown_test.go
+++ mss-boot/core/server/shutdown_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 )
 
@@ -54,5 +55,30 @@ func TestServerIgnoresMatchingContextErrorDuringShutdown(t *testing.T) {
 
 	if err := receiveError(t, done); err != nil {
 		t.Fatalf("expected matching context error to be ignored, got %v", err)
+	}
+}
+
+func TestServerIgnoresWrappedContextErrorDuringShutdown(t *testing.T) {
+	manager := New(WithoutSignalHandling())
+	started := make(chan struct{})
+	manager.Add(&testRunnable{
+		name: "worker",
+		start: func(ctx context.Context) error {
+			close(started)
+			<-ctx.Done()
+			return fmt.Errorf("worker stopped: %w", ctx.Err())
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	done := make(chan error, 1)
+	go func() {
+		done <- manager.Start(ctx)
+	}()
+	<-started
+	cancel()
+
+	if err := receiveError(t, done); err != nil {
+		t.Fatalf("expected wrapped context error to be ignored, got %v", err)
 	}
 }
